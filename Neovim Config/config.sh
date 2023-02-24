de739cf1c0f491e184b7d364a2f19e97fcde3433
J-hoplin1 <jhoplin7259@gmail.com>
2023-02-25 02:49:36 +0900

    add neovim config

--- Neovim Config/config.sh
+++ Neovim Config/config.sh
@@ -10,6 +10,10 @@ brew install llvm
 
 echo 'export PATH="/opt/homebrew/opt/llvm/bin:$PATH"' >> ~/.zshrc
 
+# Require pip3
+pip3 install python-language-server==0.20.0
+pip3 install --user pynvim
+
 # Install VIM-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
