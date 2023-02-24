7d5564f9765a892acf34becc239d578bfccbc2bc
J-hoplin1 <jhoplin7259@gmail.com>
2023-02-25 02:20:22 +0900

    add neovim config

---
+++ Neovim Config/Readme.md
@@ -0,0 +1,34 @@
+Neovim Config
+===
+
+Neovim Config script for Mac OS. Run command under below for installation and configuration
+
+```bash
+# If you were in directory of My-MacOS-Config/Neovim Config
+
+bash config.sh
+```
+## Shortcuts
+
+- `<F6>` : `gg=G`, auto indent code
+- `<F7>` : `:e`, open new file
+- `<F8>` : `:TagbarToggle`, Open tagbar
+- `<F9>` : `:NERDTreeToggle`, Open nerdtreetoogle
+
+## Plugins
+
+List of plugin as default installation
+
+```vim
+Plug 'preservim/nerdtree'
+Plug 'Xuyuanp/nerdtree-git-plugin'
+
+" tag displayer
+Plug 'preservim/tagbar'
+
+Plug 'vim-airline/vim-airline'
+Plug 'neoclide/coc.nvim'
+
+" Rainbow Bracket
+Plug 'frazrepo/vim-rainbow'
+```
---
+++ Neovim Config/config.sh
@@ -0,0 +1,137 @@
+#!/bin/zsh
+
+# Install neovim with brew
+brew install neovim
+# Install node.js
+brew install node
+brew install yarn
+brew install universal-ctags
+brew install llvm
+
+echo 'export PATH="/opt/homebrew/opt/llvm/bin:$PATH"' >> ~/.zshrc
+
+# Install VIM-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# mkdir ~/.config/nvim
+cat >> ~/.config/nvim/init.vim<<EOF
+call plug#begin('~/.vim/plugged')
+
+" tree explorer
+Plug 'preservim/nerdtree'
+Plug 'Xuyuanp/nerdtree-git-plugin'
+
+" tag displayer
+Plug 'preservim/tagbar'
+
+Plug 'vim-airline/vim-airline'
+Plug 'neoclide/coc.nvim'
+
+" Rainbow Bracket
+Plug 'frazrepo/vim-rainbow'
+
+call plug#end()
+
+set encoding=utf-8
+
+" Keymap
+nmap <F6> gg=G
+nmap <F7> :e
+nmap <F8> :TagbarToggle
+nmap <F9> :NERDTreeToggle
+
+let g:rainbow_active = 1
+EOF
+
+nvim -c ':PlugInstall' -c ':CocConfig'  -c ':q!' -c ':q!'
+
+cat >> ~/.config/nvim/coc-settings.json<<EOF
+{
+  "languageserver": {
+      "golang": {
+            "command": "gopls",
+            "rootPatterns": ["go.mod", ".vim/", ".git/", ".hg/"],
+            "filetypes": ["go"]
+          },
+      "ccls": {
+            "command": "ccls",
+            "filetypes": ["c", "cpp", "objc", "objcpp"],
+            "rootPatterns": [".ccls", "compile_commands.json", ".vim/", ".git/", ".hg/"],
+            "initializationOptions": {
+                     "cache": {
+                                "directory": "/tmp/ccls"
+                              }
+                   }
+           },
+      "python": {
+         "command": "python",
+         "args": [
+              "-mpyls",
+              "-vv",
+              "--log-file",
+              "/tmp/lsp_python.log"
+            ],
+         "trace.server": "verbose",
+         "filetypes": [
+              "python"
+            ],
+         "settings": {
+              "pyls": {
+                     "enable": true,
+                     "trace": {
+                              "server": "verbose"
+                            },
+                     "commandPath": "",
+                     "configurationSources": [
+                              "pycodestyle"
+                            ],
+                     "plugins": {
+                              "jedi_completion": {
+                                         "enabled": true
+                                       },
+                              "jedi_hover": {
+                                         "enabled": true
+                                       },
+                              "jedi_references": {
+                                         "enabled": true
+                                       },
+                              "jedi_signature_help": {
+                                         "enabled": true
+                                       },
+                              "jedi_symbols": {
+                                         "enabled": true,
+                                         "all_scopes": true
+                                       },
+                              "mccabe": {
+                                         "enabled": true,
+                                         "threshold": 15
+                                       },
+                              "preload": {
+                                         "enabled": true
+                                       },
+                              "pycodestyle": {
+                                         "enabled": true
+                                       },
+                              "pydocstyle": {
+                                         "enabled": false,
+                                         "match": "(?!test_).*\\.py",
+                                         "matchDir": "[^\\.].*"
+                                       },
+                              "pyflakes": {
+                                         "enabled": true
+                                       },
+                              "rope_completion": {
+                                         "enabled": true
+                                       },
+                              "yapf": {
+                                         "enabled": true
+                                      }
+                           }
+                  }
+           }
+      }
+    }
+}
+EOF
+
+nvim -c ':CocInstall coc-python' -c ':CocInstall coc-clangd' -c ':CocInstall coc-pyright' -c ':CocInstall coc-tabnine' -c ':CocInstall coc-json' -c ':q!' -c ':-q!' -c ':q!' -c ':-q!'
